78e07287f26bff88e65146a214f8a000c69ebbe1
Amitmaz96 <amitm9088@gmail.com>
2022-06-01 16:08:48 +0530

    my working

---
+++ src/routes/log.js
@@ -0,0 +1,8 @@
+const endpoint = "https://google.com"
+
+const log = function(){
+    console.log("this is log function")
+}
+
+module.exports.endpointurl = endpoint
+module.exports.logfunc = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const logmod = require('./log');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+
+router.get('/test-log' , function(req , res){
+    logmod.logfunc();
+    res.send(`the endpoint url is ${logmod.endpointurl}`);
+})
 
 module.exports = router;
 // adding this comment for no reason
