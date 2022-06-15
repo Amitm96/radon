284560655312c6baa15a3b0bcc44cac7005ca778
Amitmaz96 <amitm9088@gmail.com>
2022-06-15 20:36:43 +0530

    performed all the the task and saving it to assignment/auth-3 branch

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,34 @@
-const authenticate = function(req, req, next) {
+const jwt = require('jsonwebtoken')
+const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
-
-    next()
+    let token = req.headers["x-Auth-token"]
+    if(!token){
+        token = req.headers["x-auth-token"]
+    }
+    if(!token) res.send({errorMessage: "token must be present in headers"})
+    else{
+        next();
+    }
+    
+    
 }
 
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
-    next()
+    let token = req.headers["x-auth-token"]
+    let encodedToken = jwt.verify(token , "functionup-radon")
+    let userId = req.params.userId
+    if(encodedToken){
+        if(encodedToken.userId === userId) next()
+        else{
+            res.send({errorMessage : "You are not authorized to access others profile and change anything"})
+        }
+    }
+    else{
+        res.send({errorMessage : "token not matched"})
+    }
 }
+
+module.exports = {authenticate , authorise}
